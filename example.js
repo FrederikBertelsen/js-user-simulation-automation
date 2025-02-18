bba4f59fa8e9dc981b4375bf8adef28ca174702e
Frederik Bertelsen <frbb@itu.dk>
2025-02-18 09:13:17 +0100

    add some helper functions

--- example.js
+++ example.js
@@ -1,18 +1,18 @@
 const { chromium } = require('playwright');
 
 (async () => {
-    const browser = await chromium.launch({ channel: 'chrome', headless: false });
-    const context = await browser.newContext();
-    const page = await context.newPage();
+  const browser = await chromium.launch({ channel: 'chrome', headless: false });
+  const context = await browser.newContext();
+  const page = await context.newPage();
 
-    await page.goto('https://imdb.com');
+  await page.goto('https://imdb.com');
 
-    await page.fill('input#suggestion-search', 'pigen med nålen');
-    await page.press('input#suggestion-search', 'Enter');
+  await page.fill('input#suggestion-search', 'pigen med nålen');
+  await page.press('input#suggestion-search', 'Enter');
 
-    await page.click('section[data-testid="find-results-section-title"] li a');
+  await page.click('section[data-testid="find-results-section-title"] li a');
 
-    await page.waitForTimeout(10000);
+  await page.waitForTimeout(10000);
 
-    await browser.close();
+  await browser.close();
 })();
