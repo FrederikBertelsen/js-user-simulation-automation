efe71c5d2763c1e20f851f0489d4638cd6c98dac
Frederik Bertelsen <frbb@itu.dk>
2025-02-19 11:07:37 +0100

    simplified simulation example

--- example.ts
+++ example.ts
@@ -3,15 +3,15 @@ import { runUserSimulation } from './runner.ts';
 
 for (var i = 0; i < 10; i++) {
   runUserSimulation(
-    /* localhost */ false,
+    /* localhost */ true,
     /* headless */ true,
     /* enable logging to console */ false,
     async (page: InteractionPage) => {
       for (var i = 0; i < 1000; i++) {
         await page.clickButton();
-        await page.checkCheckbox();
-        await page.selectRadioButton();
-        await page.selectFromDropdown();
+        // await page.checkCheckbox();
+        // await page.selectRadioButton();
+        // await page.selectFromDropdown();
       }
     });
 }
